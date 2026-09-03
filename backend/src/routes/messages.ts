9db7ac8929a3438fc9ec78028f53c934a9e82290
yatfu <55002354+yatfu@users.noreply.github.com>
2026-09-03 03:54:56 -1000

    feat: delete request handler
    for users wanting to delete their own messages

--- backend/src/routes/messages.ts
+++ backend/src/routes/messages.ts
@@ -4,6 +4,7 @@ import {pool} from '../db';
 
 const router = express.Router();
 
+// GET messages
 router.get("/:chatroomId", async (req, res) => {
   const { chatroomId } = req.params;
   // check if chatroom exists, if not throw 404
@@ -20,5 +21,17 @@ router.get("/:chatroomId", async (req, res) => {
   );
   res.json(result.rows);
 });
+
+// DELETE message given id
+router.delete("/:messageId", async (req, res) => {
+  const { messageId } = req.params;
+  const result = await pool.query(
+    `DELETE FROM messages WHERE id = $1 RETURNING *`, [messageId]
+  );
+  if (result.rowCount === 0) {
+    return res.status(404).json({error: "Message not found"});
+  }
+  res.status(204).send();
+})
 
 export default router;
