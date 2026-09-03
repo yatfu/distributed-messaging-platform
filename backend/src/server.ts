19d60e404e3c0538aa687798283d67df17909d52
yatfu <55002354+yatfu@users.noreply.github.com>
2026-09-02 16:36:53 -1000

    create: chatrooms router

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,16 +9,20 @@ const port = Number(process.env.PORT) || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/health",  async (_req, res) => {
+app.get("/api/health", async (_req, res) => {
   try {
     await pool.query("SELECT 1");
-    res.json({status: "ok"});
-  }
-  catch (error) {
-    res.status(500).json({status: "error", database: "disconnected"})
+    res.json({ status: "ok" });
+  } catch (error) {
+    res.status(500).json({ status: "error", database: "disconnected" });
   }
 });
+
+const chatroomsRouter = require("./routes/chatrooms.js");
+app.use("/chatrooms", chatroomsRouter);
 
 app.listen(port, () => {
   console.log(`Backend running at http://localhost:${port}`);
 });
+
+// Error handler
