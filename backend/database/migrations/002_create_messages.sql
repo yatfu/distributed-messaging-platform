25c3f962bd028f49faeea2fcfd563b89fba30957
yatfu <55002354+yatfu@users.noreply.github.com>
2026-09-03 03:52:48 -1000

    Update 002_create_messages.sql
    added edited_at variable to be able to display when message was last edited

--- backend/database/migrations/002_create_messages.sql
+++ backend/database/migrations/002_create_messages.sql
@@ -4,6 +4,7 @@ CREATE TABLE messages (
     sender_id UUID NOT NULL,
     content TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    edited_at TIMESTAMPTZ
 
     CONSTRAINT fk_chatroom
         FOREIGN KEY (chatroom_id)
