527ef361b0ab5f1f7a3208ace4690bd455b6a45e
yatfu <55002354+yatfu@users.noreply.github.com>
2026-09-14 18:26:18 -1000

    create: POST message endpoint
    given user session token and chatroom id

--- backend/src/routes/messages.ts
+++ backend/src/routes/messages.ts
@@ -1,28 +1,50 @@
 import express from "express";
-import { pool } from "../db";
-import { validateUuid } from "../lib/validate";
+import { pool } from "../db.js";
+import { validateUuid, validateString } from "../lib/validate.js";
 import { getUserFromToken } from "../lib/auth.js";
+import crypto from "node:crypto";
 
 const router = express.Router();
 
 // CREATE message given user session token and chatroom id
 router.post("/", async (req, res) => {
-  const 
+  //validate chatroom
+  const validChatroomId = validateUuid(req.body?.room, "chatroomId"); // req.body? checks if body exists, if exists then gets room
+  //validate user from token
+  const validUser = await getUserFromToken(req.cookies.sessionToken);
+  //validate message
+  const validMessage = validateString(req.body?.message, "message");
+  //create message
+  const messageId = crypto.randomUUID();
+  const result = await pool.query(`
+    INSERT into MESSAGES (id, chatroom_id, sender_id, content)
+    SELECT $1, id, $3, $4 FROM chatrooms
+    WHERE id = $2
+      AND expires_at > NOW()
+    RETURNING id, chatroom_id, sender_id, content, created_at;
+  `, [messageId, validChatroomId, validUser.id, validMessage]) // INSERT -> SELECT inserts only after select
+  //check to see if message was inserted
+  if (result.rows.length === 0) {
+    return res.status(404).json({ error: "Chatroom expired or not found"}); //404: chatroom not found or expired
+  }
+  return res.status(201).json({ message: result.rows[0]}); // 201: message created
 });
-// EDIT message given message id and user session token
+// EDIT message given message id and user session token - NOT MVP STAGE
 
 // DELETE message given message id and user session token from cookies
 router.delete("/:messageId", async (req, res) => {
   //validate message id, token id
   const { messageId } = req.params;
-  const validMessageId = validateUuid(messageId, "message");
+  const validMessageId = validateUuid(messageId, "messageId");
 
   const token = req.cookies.sessionToken;
   const validUser = await getUserFromToken(token);
   const validUserId = validUser.id;
 
   const result = await pool.query(
-    `DELETE FROM messages WHERE id = $1 AND sender_id = $2 RETURNING *`,
+    `DELETE FROM messages
+     WHERE id = $1 AND sender_id = $2
+     RETURNING id, chatroom_id, sender_id, content, created_at`,
     [validMessageId, validUserId]
   );
   if (result.rowCount === 0) {
