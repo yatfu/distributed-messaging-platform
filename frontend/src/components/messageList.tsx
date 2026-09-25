1b7e53145cd8bff29a5258f9bd941dec65b0af14
yatfu <55002354+yatfu@users.noreply.github.com>
2026-09-25 00:39:18 -1000

    create: messageList hardcode

---
+++ frontend/src/components/messageList.tsx
@@ -0,0 +1,11 @@
+export default function MessageList({ children }) {
+  return (
+    <div id="message-list">
+      <div className="message">
+        <h2 className="message-time"> Sept. 09 2026 8:13 P.M.</h2>
+        <h2 className="message-name"> Keanu</h2>
+        <p className="message-text">Hi!</p>
+      </div>
+    </div>
+  );
+}
